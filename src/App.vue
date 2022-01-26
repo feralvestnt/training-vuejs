a2b74875721f3da2d476d7bff4b740552641f005
fernando.alves@tabit-gmbh.de <fernando.alves@tabit-gmbh.de>
2022-01-26 14:02:54 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,152 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!--<div>
+    Root Foo: {{rootFoo}} <br />
+    Robots Foo: {{robotsFoo}} <br />
+    Users Foo: {{usersFoo}} <br />
+    <br />
+    Root Getter Foo: {{rootGetterFoo}} <br />
+    Robots Getter Foo: {{robotsGetterFoo}} <br />
+  </div>-->
+  <header>
+    <nav>
+      <ul>
+        <li class="nav-item" >
+          <router-link class="nav-link" :to="{name: 'Home'}" exact>
+          <img class="logo" src="./assets/build-a-bot-logo.png" />
+          build-a-bot
+          </router-link>
+        </li>
+        <li class="nav-item" >
+          <router-link class="nav-link" :to="{name: 'Build'}" exact>
+            Build
+          </router-link>
+        </li>
+        <li class="nav-item cart" >
+          <router-link class="nav-link" to="/cart" exact>
+            Cart
+          </router-link>
+          <div class="cart-items">
+            {{cart.length}}
+          </div>
+        </li>
+        <li>
+          User: {{user.userName}}
+          <button @click="changeUserName()" > Change User</button>
+        </li>
+      </ul>
+    </nav>
+  </header>
+  <div class="container">
+    <aside class="aside" > 
+      <router-view name="sidebar" />
+    </aside>
+    <main>
+      <router-view/>
+    </main>
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+
+import { mapState, mapGetters } from 'vuex';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
+  computed: {
+    ...mapState({ 
+      rootFoo: 'foo', 
+      usersFoo: state => state.users.foo
+    }),
+    ...mapState('robots', { robotsFoo: 'foo' }), // this sintax works only when a module has namespace defined
+    ...mapGetters({ rootGettersFoo: 'foo' }),
+    ...mapGetters('robots', { robotsGetterFoo: 'foo' }),
+    cart() {
+      return this.$store.state.robots.cart;
+    }
   },
+  data() {
+    return {
+      user: { userName: 'Fernando Henrique Alves'},
+    };
+  },
+  provide() {
+    return { user: this.user };
+  },
+  methods: {
+    changeUserName() {
+      this.user.userName = 'Teste User';
+    }
+  }
 };
 </script>
 
 <style>
+body {
+  background: #999;
+  background-attachment: fixed;
+}
+</style>
+
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+}
+main {
+  padding: 30px;
+  background-color: white;
+  width: 964px;
+  min-height: 300px;
+}
+header {
+  background-color: #999;
+  width: 1184px;
+  margin: 0 auto;
+}
+ul {
+  padding: 3px;
+  display: flex;
+}
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+.logo {
+  vertical-align: middle;
+  height: 30px;
+}
+.nav-link {
+  text-decoration: none;
+  color: inherit;
+}
+.router-link-active {
+  color: white;
+}
+.container {
+  display: flex;
+  margin: 10px auto 0 auto;
+  justify-content: center;
+}
+.aside {
+  padding: 30px;
+  background-color: #aaa;
+  width: 100px;
+  min-height: 300px;
+}
+.nav-item.cart {
+  position: relative;
+  margin-left: auto;
+  border-right: none;
+}
+.cart-items {
+  position: absolute;
+  top: -5px;
+  right: -9px;
+  font-size: 18px;
+  width: 20px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: inline-block;
+  border-radius: 100px;
+  background-color: mediumseagreen;
 }
 </style>
---
+++ src/router/index.js
@@ -0,0 +1,72 @@
+import { 
+    createRouter,
+    createWebHistory,
+ } from 'vue-router';
+import HomePage from '../home/HomePage.vue';
+import RobotBuilder from '../build/RobotBuilder.vue';
+import BrowseParts from '../parts/BrowseParts.vue';
+import RobotHeads from '../parts/RobotHeads.vue';
+import RobotArms from '../parts/RobotArms.vue';
+import RobotTorsos from '../parts/RobotTorsos.vue';
+import RobotBases from '../parts/RobotBases.vue';
+import SidebarStandard from '../sidebars/SidebarStandard';
+import SidebarBuild from '../sidebars/SidebarBuild';
+import ShoppingCart from '../cart/ShoppingCart';
+
+export default createRouter({
+    history: createWebHistory(),
+    routes: [{
+        path:'/',
+        name: 'Home',
+        components: {
+            default: HomePage,
+            sidebar: SidebarStandard,
+        },
+    }, {
+        path:'/build',
+        name: 'Build',
+        components: {
+            default: RobotBuilder,
+            sidebar: SidebarBuild,
+        },
+    }, {
+        
+        path: '/parts/browse',
+        name: 'BrowseParts',
+        component: BrowseParts,
+        children: [
+            {
+                name: 'BrowseHeads',
+                path: 'heads',
+                component: RobotHeads,
+            },
+            {
+                name: 'BrowseArms',
+                path: 'arms',
+                component: RobotArms,
+            },
+            {
+                name: 'BrowseTorsos',
+                path: 'torsos',
+                component: RobotTorsos,
+            },
+            {
+                name: 'BrowseBases',
+                path: 'bases',
+                component: RobotBases,
+            },
+        ]
+    }, {
+        path:'/parts/:partType/:id',
+        name: 'Parts',
+        component: RobotBuilder,
+        beforeEnter(to, from, next) {
+            const isValidId = Number.isInteger(Number(to.params.id));
+            next(isValidId);
+        }
+    }, {
+        path: '/cart',
+        name: 'Cart',
+        component: ShoppingCart,
+    }],
+});
